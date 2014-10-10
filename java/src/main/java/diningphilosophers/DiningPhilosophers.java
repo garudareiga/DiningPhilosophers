ca10ba032c3318bed49427b604923cf41f7007bf
Ray Chen <garudareiga@gmail.com>
2014-10-10 00:09:57 -0700

    init java solution

---
+++ java/src/main/java/diningphilosophers/DiningPhilosophers.java
@@ -0,0 +1,17 @@
+package diningphilosophers;
+
+/**
+ * Created by raychen on 10/10/14.
+ */
+public class DiningPhilosophers {
+    // The number of philosophers
+    private static final int NUM_PHILOSOPHERS = 5;
+
+    /**
+     * Test the dining philosophers solution
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java/src/main/java/diningphilosophers/Philosopher.java
@@ -0,0 +1,24 @@
+package diningphilosophers;
+
+/**
+ * Created by raychen on 10/10/14.
+ */
+
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * A philosopher alternates between thinking and eating.
+ */
+public class Philosopher implements Runnable {
+
+    public Philosopher() {
+
+    }
+
+
+    @Override
+    public void run() {
+
+    }
+}
